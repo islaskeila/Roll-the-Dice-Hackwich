23f31edc192fd74c33033ffd383346994c43e737
Keila Islas <kislas@bsd220.org>
2023-10-25 09:47:29 -0500

    Initial Commit

---
+++ Roll the Dice Hackwich/Roll_the_Dice_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_Dice_HackwichApp.swift
+//  Roll the Dice Hackwich
+//
+//  Created by Keila Islas on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_Dice_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
